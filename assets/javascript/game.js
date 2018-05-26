c285dcf8581e894a7d32b987cb08824cc56d3117
Angel Lugo <this@angellugo.com>
2018-05-26 16:37:05 -0700

    CSS Enhanced working version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,36 +3,38 @@ var debug = false;
 var wins = 0;
 var losses = 0;
 var randomNumber;
-var totalScore;
+var totalPoints;
 var rubyPoints;
 var diamondPoints;
 var yellowDiamondPoints;
 var emeraldPoints;
 
 function resetGame() {
-    randomNumber = Math.floor(Math.random() * (120 - 19 + 1) ) + 19;
-    totalScore = 0;
-    rubyPoints = Math.floor(Math.random() * 12) + 1;
-    diamondPoints = Math.floor(Math.random() * 12) + 1;
-    yellowDiamondPoints = Math.floor(Math.random() * 12) + 1;
-    emeraldPoints = Math.floor(Math.random() * 12) + 1;
-
     debug && console.log("__________________________Game Reset_________________________");
+    randomNumber = Math.floor(Math.random() * (120 - 19 + 1) ) + 19;
     debug && console.log("randomNumber", randomNumber);
+    totalPoints = 0;
+    rubyPoints = Math.floor(Math.random() * 12) + 1;
     debug && console.log("rubyPoints", rubyPoints);
+    diamondPoints = Math.floor(Math.random() * 12) + 1;
     debug && console.log("diamondPoints", diamondPoints);
+    yellowDiamondPoints = Math.floor(Math.random() * 12) + 1;
     debug && console.log("yellowDiamondPoints", yellowDiamondPoints);
+    emeraldPoints = Math.floor(Math.random() * 12) + 1;
     debug && console.log("emeraldPoints", emeraldPoints);
 
-    document.querySelector("#randomNumber").innerHTML = "Random number: " + randomNumber;
-    document.querySelector("#wins").innerHTML = "Wins: " + wins;
-    document.querySelector("#losses").innerHTML = "Losses: " + losses;
-    renderTotalScore();
+    // document.querySelector("#randomNumber").innerHTML = "Random number: " + randomNumber;
+    $('#randomNumber').text("Random number: " + randomNumber);
+    // document.querySelector("#wins").innerHTML = "Wins: " + wins;
+    $('#wins').text("Wins: " + wins);
+    // document.querySelector("#losses").innerHTML = "Losses: " + losses;
+    $('#losses').text("Losses: " + losses);
+    renderTotalPoints();
 
 };
 
-function renderTotalScore() {
-    document.querySelector("#totalScore").innerHTML = "Total Score: " + totalScore;
+function renderTotalPoints() {
+    document.querySelector("#totalPoints").innerHTML = "Total Points: " + totalPoints;
 }
 
 function playerWins() {
@@ -48,35 +50,35 @@ function playerLoses() {
 }
 
 function checkStatusofGame() {
-    if (totalScore > randomNumber){
+    if (totalPoints > randomNumber){
         playerLoses();
-    }else if ( totalScore === randomNumber){
+    }else if ( totalPoints === randomNumber){
         playerWins();
     }
     //else continue playing
 }
 
 resetGame();
-$('.rubiesButton').on("click", function(event){
-    totalScore += rubyPoints;
-    renderTotalScore();
+$('#rubiesButton').on("click", function(event){
+    totalPoints += rubyPoints;
+    renderTotalPoints();
     checkStatusofGame();
 });
 
-$('.diamondButton').on("click", function(event){
-    totalScore += diamondPoints;
-    renderTotalScore();
+$('#diamondButton').on("click", function(event){
+    totalPoints += diamondPoints;
+    renderTotalPoints();
     checkStatusofGame();
 });
 
-$('.yellowDiamondButton').on("click", function(event){
-    totalScore += yellowDiamondPoints;
-    renderTotalScore();
+$('#yellowDiamondButton').on("click", function(event){
+    totalPoints += yellowDiamondPoints;
+    renderTotalPoints();
     checkStatusofGame();
 });
 
-$('.emeraldButton').on("click", function(event){
-    totalScore += emeraldPoints;
-    renderTotalScore();
+$('#emeraldButton').on("click", function(event){
+    totalPoints += emeraldPoints;
+    renderTotalPoints();
     checkStatusofGame();
 });
